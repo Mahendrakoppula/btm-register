89e642f7053e5f5fffa30f6ef46ec303e9068f61
Mahendra koppula <mahendrakoppula@Users-MacBook-Pro.local>
2023-09-28 22:37:38 +0530

    btm-register

---
+++ utils/str-operations.ts
@@ -0,0 +1,22 @@
+export const acceptString = (value: string) => {
+  // Accept only string
+  const regex = new RegExp(/^[a-zA-Z\s]*$/);
+  return regex.test(value);
+};
+
+export const acceptNumbers = (value: string) => {
+  // Accept only numbers
+  const regex = new RegExp(/^[0-9]*$/);
+  return regex.test(value);
+};
+
+export const removeSpace = (value: string) => {
+  // Remove all spaces
+  const regex = new RegExp(/\s/g);
+  return value.replace(regex, '');
+};
+
+export function isUrl(string: any) {
+  const regex = RegExp(/^(https?| ftp):\/\/[^\s/$.?#/][^\s]*$/i);
+  return regex.test(string);
+}
